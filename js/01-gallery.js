bf22da05d149b7c4010a3e0c53695a1ded600ae0
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-12 22:38:31 +0200

    backup save

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".js-gallery");
+// console.dir(gallery);
+
+gallery.addEventListener("click", onGallerryClick);
+
+const galleryItemMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}.jpg"
+      alt="${description}}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+// console.log(galleryItemMarkup);
+gallery.insertAdjacentHTML("afterbegin", galleryItemMarkup);
+
+function onGallerryClick(event) {
+  event.preventDefault();
+  console.log(event.target);
+}

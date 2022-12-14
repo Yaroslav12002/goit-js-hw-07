1fe89c5abb0fe7a09578de8508e70eee44d73e91
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-14 19:23:25 +0200

    task 01 ready
    task 01 ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,54 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const gallery = document.querySelector(".js-gallery");
-// console.dir(gallery);
+const galleryImageMarkup = createGalleryImagesMarkup(galleryItems);
+let modalImage = null;
 
-gallery.addEventListener("click", onGallerryClick);
+gallery.innerHTML = galleryImageMarkup;
+gallery.addEventListener("click", onGallerryImageClick);
 
-const galleryItemMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
+function createGalleryImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
   <a class="gallery__link" href="${original}.jpg">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}.jpg"
+      data-source="${original}"
       alt="${description}}"
     />
   </a>
 </div>`;
-  })
-  .join("");
+    })
+    .join("");
+}
 
-// console.log(galleryItemMarkup);
-gallery.insertAdjacentHTML("afterbegin", galleryItemMarkup);
-
-function onGallerryClick(event) {
+function onGallerryImageClick(event) {
   event.preventDefault();
-  console.log(event.target);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const activeImageURL = event.target.dataset.source;
+  modalImage = basicLightbox.create(`<img src="${activeImageURL}">`);
+  modalImage.show(addKeyboardListenerToGallery);
+}
+
+function onModalKeypressed(event) {
+  // console.log("key: ", event.key);
+  if (event.key !== "Escape") {
+    return;
+  }
+
+  modalImage.close(removeKeyboardListenerFromGallery);
+}
+
+function addKeyboardListenerToGallery() {
+  gallery.addEventListener("keydown", onModalKeypressed);
+}
+
+function removeKeyboardListenerFromGallery() {
+  gallery.removeEventListener("keydown", onModalKeypressed);
 }

f59108fe64211778f1540e5c1d1253b2e6f600fc
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-14 21:15:20 +0200

    task 2 ready
    task 2 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".js-gallery");
+const galleryImageMarkup = createGalleryImagesMarkup(galleryItems);
+
+gallery.innerHTML = galleryImageMarkup;
+
+const lightboxGallery = new SimpleLightbox(".js-gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a></li>`;
+    })
+    .join("");
+}
